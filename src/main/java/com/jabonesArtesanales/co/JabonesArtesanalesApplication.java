02ecafcb9cbd216aa19d36f95ee1abf64625abdc
diocelly <diocellydiazc93@gmail.com>
2025-10-05 22:17:23 -0500

    Diocelly - Actualizacion  del Application del proyecto

--- src/main/java/com/jabonesArtesanales/co/JabonesArtesanalesApplication.java
+++ src/main/java/com/jabonesArtesanales/co/JabonesArtesanalesApplication.java
@@ -1,9 +1,17 @@
 package com.jabonesArtesanales.co;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+	    "com.jabonesArtesanales.co.domain",     // <-- ajusta al paquete real de tus @Entity
+	})
+	@EnableJpaRepositories(basePackages = {
+	    "com.jabonesArtesanales.co.repository"  // <-- ajusta al paquete real de tus repos
+	})
 public class JabonesArtesanalesApplication {
 
 	public static void main(String[] args) {
